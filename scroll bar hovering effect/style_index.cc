0df580a6313b85486264ac07fa0706adb303d182
Shreya Agarwal <46715038+shreyaagr2909@users.noreply.github.com>
2020-05-11 10:20:03 +0530

    Create style_index.cc

---
+++ scroll bar hovering effect/style_index.cc
@@ -0,0 +1,45 @@
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: consolas;
+}
+
+body{
+    background:#000 ;
+}
+
+section{
+    width: 100%;
+    min-height: 1000vh;
+    padding: 100px;
+    
+}
+
+section h1{
+    font-size: 4em;
+    color: #666;
+    margin-bottom: 20px;
+}
+
+section p{
+    font-size: 1.2em;
+    color: #666;
+}
+
+::-webkit-scrollbar{
+    width: 12px;
+    height: 100px;
+}
+
+::-webkit-scrollbar-thumb{
+
+    height: 30%;
+    background:linear-gradient(transparent, #30ff00);
+    border-radius: 3px;
+}
+
+::-webkit-scrollbar-thumb:hover{
+    background: linear-gradient(transparent , #00c6ff);
+}
+
